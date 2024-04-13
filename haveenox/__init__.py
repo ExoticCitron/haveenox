7cd7ce35de66d2548e1b6e5a89b6e0bf8eac1d1c
Exo <127617294+ExoticCitron@users.noreply.github.com>
2024-04-13 15:40:09 +0100

    Add dict

--- haveenox/__init__.py
+++ haveenox/__init__.py
@@ -1,7 +1,16 @@
-GREEN = '\033[92m'
-RED = '\033[91m'
-YELLOW = '\033[93m'
-# Add more colors as needed
-RESET = '\033[0m'
+# __init__.py
 
-from .haveenox import * 
+# Define the color codes and names
+COLORS = {
+    'GREEN': '\033[92m',
+    'RED': '\033[91m',
+    'YELLOW': '\033[93m',
+    # Add more colors as needed
+    'RESET': '\033[0m'
+}
+
+# Dynamically create the color variables
+for color_name, color_code in COLORS.items():
+    globals()[color_name] = color_code
+
+from .haveenox import *
