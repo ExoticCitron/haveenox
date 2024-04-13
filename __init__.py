8874c6796fd06d0079126e24c459a964c734a175
Exo <127617294+ExoticCitron@users.noreply.github.com>
2024-04-13 17:04:44 +0100

    more ANSI escape sequences

--- __init__.py
+++ __init__.py
@@ -1,8 +1,21 @@
-GREEN = '\033[92m'
-RED = '\033[91m'
-YELLOW = '\033[93m'
-
 RESET = '\033[0m'
+BLACK = '\033[30m' 
+RED = '\033[31m'
+GREEN = '\033[32m' 
+YELLOW = '\033[33m'
+BLUE = '\033[34m' 
+MAGENTA = '\033[35m' 
+CYAN = '\033[36m' 
+WHITE = '\033[37m' 
+LIGHTBLACK = '\033[90m' 
+LIGHTRED = '\033[91m' 
+LIGHTGREEN = '\033[92m' 
+LIGHTYELLOW = '\033[93m' 
+LIGHTBLUE = '\033[94m' 
+LIGHTMAGENTA = '\033[95m' 
+LIGHTCYAN = '\033[96m' 
+LIGHTWHITE = '\033[97m' 
+# more soon babagrill
 
 
 from .haveenox import *
