49056d65d6040a569d66050ea70350388defdd1e
Exo <127617294+ExoticCitron@users.noreply.github.com>
2024-04-13 16:56:01 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,11 +1,9 @@
-# __init__.py
-
-# Define ANSI color codes
 GREEN = '\033[92m'
 RED = '\033[91m'
 YELLOW = '\033[93m'
-# Add more colors as needed
+
 RESET = '\033[0m'
+
 
 from .haveenox import *
 
