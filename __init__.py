1266ff51aabe9c2a13c95cd422362f470b449a93
Exo <127617294+ExoticCitron@users.noreply.github.com>
2024-04-13 19:17:45 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -50,5 +50,6 @@ def repeat(string, times):
 
 from .dcPost import dcPost
 from .parse_json import parse_json
+from .create import create
 
-__all__ = ['dcPost', 'clr', 'repeat', 'RESET', 'BLACK', 'RED', 'GREEN', 'YELLOW', 'BLUE', 'MAGENTA', 'CYAN', 'WHITE', 'LIGHTBLACK', 'LIGHTRED', 'LIGHTGREEN', 'LIGHTYELLOW', 'LIGHTBLUE', 'LIGHTMAGENTA', 'LIGHTCYAN', 'LIGHTWHITE', 'parse_json']
+__all__ = ['dcPost', 'clr', 'repeat', 'RESET', 'BLACK', 'RED', 'GREEN', 'YELLOW', 'BLUE', 'MAGENTA', 'CYAN', 'WHITE', 'LIGHTBLACK', 'LIGHTRED', 'LIGHTGREEN', 'LIGHTYELLOW', 'LIGHTBLUE', 'LIGHTMAGENTA', 'LIGHTCYAN', 'LIGHTWHITE', 'parse_json', 'create'] 
